8ed6e5d00afd31298884b824ebdcd3f4d274a9fa
VladUPC <vladyslav.barzion@estudiantat.upc.edu>
2024-05-09 20:09:35 +0200

    Muestra Todas Actividades

--- DjangoBack/activities/views.py
+++ DjangoBack/activities/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import api_view, authentication_classes
 from .serializer import ActivitySerializer
 from rest_framework import status
 from web_user.authentication import CookieTokenAuthentication
+from .models import Activity
 
 
 
@@ -17,3 +18,23 @@ def create_activity(request):
         serializer.save()
         return Response({'status': 'success'},status=status.HTTP_201_CREATED)
     return Response({"Error": serializer.errors},status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["GET"])
+@authentication_classes([CookieTokenAuthentication])
+def all_activities(request):
+
+    if not request.user.is_authenticated:
+        return Response({"error": "NO autenticado"}, status=status.HTTP_403_FORBIDDEN)
+    user = request.user
+    
+    if not user.is_admin:
+        return Response({"error": "No es admin"}, status= status.HTTP_403_FORBIDDEN)
+
+
+
+    activities = Activity.objects.all()
+
+    serializer = ActivitySerializer(activities, many = True)
+
+    return Response(serializer.data, status=status.HTTP_200_OK)
