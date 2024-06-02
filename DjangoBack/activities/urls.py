d4f2874b3a19a19b903c773539b5fbf03c68f511
VladUPC <vladyslav.barzion@estudiantat.upc.edu>
2024-06-03 01:07:07 +0200

    add archivos para todos los roles

--- DjangoBack/activities/urls.py
+++ DjangoBack/activities/urls.py
@@ -8,7 +8,8 @@ urlpatterns = [
     path("mayores_inscription/", views.mayores_inscription),
     path("lider_inscription/", views.lider_inscription),
     path("monitor_inscription/", views.monitor_inscription),
-    path("get_or_create_inscription/<str:role>", views.get_or_create_inscription),
-    
+    path("get_or_create_inscription/<str:role>",
+         views.get_or_create_inscription),
+
 
 ]
--- DjangoBack/web_user/views.py
+++ DjangoBack/web_user/views.py
@@ -1,4 +1,4 @@
-from rest_framework.response import Response 
+from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from .serializer import *
 from rest_framework.decorators import api_view, authentication_classes
@@ -10,12 +10,9 @@ from .authentication import CookieTokenAuthentication
 from django.contrib.auth.hashers import check_password, make_password
 
 
-
-
-
 @api_view(['POST'])
 def register(request):
-        
+
     user_data = request.data
 
     # Si no hay usuarios en el sistema, crear el primer usuario como administrador
@@ -29,44 +26,44 @@ def register(request):
             user.save()
             return Response(user_serializer.data, status=status.HTTP_201_CREATED)
         else:
-            return Response({"Error": user_serializer.errors},status=status.HTTP_400_BAD_REQUEST)
-     
+            return Response({"Error": user_serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
     # Si ya hay usuarios en el sistema, crear una solicitud pendiente
     serializer = Web_User_Pending_Serializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
-        return Response(serializer.data ,status=status.HTTP_201_CREATED, )
-    return Response({"Error": serializer.errors},status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, )
+    return Response({"Error": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
 
 @api_view(['POST'])
 def login(request):
     user = get_object_or_404(User, email=request.data["email"])
-    
+
     if not user.check_password(request.data["password"]):
-        return JsonResponse({"error" : "Invalid password"}, status=status.HTTP_404_NOT_FOUND)
-    
+        return JsonResponse({"error": "Invalid password"}, status=status.HTTP_404_NOT_FOUND)
+
     token, created = Token.objects.get_or_create(user=user)
     is_admin = user.is_admin
     response = JsonResponse({"is_admin": is_admin})
-    #Guarda token en las cookies
+    # Guarda token en las cookies
     response.set_cookie("token", token.key, httponly=True,  samesite='Lax')
 
     return response
 
 
-
 @api_view(["POST"])
 @authentication_classes([CookieTokenAuthentication])
 def logout(request):
-    #Elimina token de BD
+    # Elimina token de BD
 
     if not request.user.is_authenticated:
         return Response({"error": "Usuario no autenticado"}, status=status.HTTP_403_FORBIDDEN)
 
-
     request.user.auth_token.delete()
 
-    response = Response({"message": "Logout exitoso"}, status=status.HTTP_200_OK)
+    response = Response({"message": "Logout exitoso"},
+                        status=status.HTTP_200_OK)
     response.delete_cookie("token")
     return response
 
@@ -78,13 +75,13 @@ def registration_requests(request):
     if not request.user.is_authenticated:
         return Response({"error": "NO autenticado"}, status=status.HTTP_403_FORBIDDEN)
     user = request.user
-    
+
     if not user.is_admin:
-        return Response({"error": "No es admin"}, status= status.HTTP_403_FORBIDDEN)
+        return Response({"error": "No es admin"}, status=status.HTTP_403_FORBIDDEN)
 
     regist_requests = Web_User_Pending.objects.all()
 
-    serializer = Web_User_NoPassword_Serializer(regist_requests, many = True)
+    serializer = Web_User_NoPassword_Serializer(regist_requests, many=True)
 
     return Response(serializer.data, status=status.HTTP_200_OK)
 
@@ -92,22 +89,22 @@ def registration_requests(request):
 @api_view(['POST'])
 @authentication_classes([CookieTokenAuthentication])
 def reject_request(request):
-    
+
     if not request.user.is_authenticated:
         return Response({"error": "NO autenticado"}, status=status.HTTP_403_FORBIDDEN)
     user = request.user
-    
+
     if not user.is_admin:
-        return Response({"error": "No es admin"}, status= status.HTTP_403_FORBIDDEN)
-    
+        return Response({"error": "No es admin"}, status=status.HTTP_403_FORBIDDEN)
+
     email = request.data["email"]
     print(request.data["reason"])
     try:
-        web_user_pending = Web_User_Pending.objects.get(email = email)
+        web_user_pending = Web_User_Pending.objects.get(email=email)
         web_user_pending.delete()
-        return Response({'status': 'success'},status=status.HTTP_200_OK)
+        return Response({'status': 'success'}, status=status.HTTP_200_OK)
     except Web_User_Pending.DoesNotExist:
-        return Response({"error": "Peticion no existe"},status=status.HTTP_404_NOT_FOUND)
+        return Response({"error": "Peticion no existe"}, status=status.HTTP_404_NOT_FOUND)
 
 
 @api_view(['POST'])
@@ -116,16 +113,16 @@ def acept_request(request):
     if not request.user.is_authenticated:
         return Response({"error": "NO autenticado"}, status=status.HTTP_403_FORBIDDEN)
     user = request.user
-    
+
     if not user.is_admin:
-        return Response({"error": "No es admin"}, status= status.HTTP_403_FORBIDDEN)
-    
+        return Response({"error": "No es admin"}, status=status.HTTP_403_FORBIDDEN)
+
     email = request.data["email"]
 
     try:
-        web_user_pending = Web_User_Pending.objects.get(email = email)
+        web_user_pending = Web_User_Pending.objects.get(email=email)
 
-        #Creamos nuevo profile
+        # Creamos nuevo profile
 
         new_profile = Profile.objects.create(
             name=web_user_pending.profile.name,
@@ -136,31 +133,30 @@ def acept_request(request):
             birthdate=web_user_pending.profile.birthdate
         )
 
-        #Creamos nuevo user
+        # Creamos nuevo user
         new_user = User.objects.create_user(
-            email = email,
-            profile = new_profile,
-            password = None
+            email=email,
+            profile=new_profile,
+            password=None
         )
         new_user.password = web_user_pending.password
         new_user.save()
 
         web_user_pending.delete()
-        return Response({'status': 'success'},status=status.HTTP_201_CREATED)
+        return Response({'status': 'success'}, status=status.HTTP_201_CREATED)
     except Web_User_Pending.DoesNotExist:
-        return Response({"error": "Peticion no existe"},status=status.HTTP_404_NOT_FOUND)
-
+        return Response({"error": "Peticion no existe"}, status=status.HTTP_404_NOT_FOUND)
 
 
 @api_view(["POST"])
 def change_password(request):
-    
+
     user = User.objects.filter(email=request.data["email"]).first()
 
     if not user:
         return Response({"error": "Usaurio con este email no existe"}, status=status.HTTP_404_NOT_FOUND)
-    
-    if not check_password(request.data["password_old"],user.password):
+
+    if not check_password(request.data["password_old"], user.password):
         return Response({"error": "Contraseña actual incorrecta"}, status=status.HTTP_400_BAD_REQUEST)
 
     user.set_password(request.data["password_new"])
@@ -171,32 +167,31 @@ def change_password(request):
 @api_view(["GET"])
 @authentication_classes([CookieTokenAuthentication])
 def info_profile(request):
-    
+
     if not request.user.is_authenticated:
         return Response({"error": "NO autenticado"}, status=status.HTTP_403_FORBIDDEN)
     user = request.user
-    
+
     profile = user.profile
     serializer = User_Profile_Serializer(profile)
 
     return Response(serializer.data, status=status.HTTP_200_OK)
 
 
-
 @api_view(["PUT"])
 @authentication_classes([CookieTokenAuthentication])
 def update_profile(request):
-    
+
     if not request.user.is_authenticated:
         return Response({"error": "NO autenticado"}, status=status.HTTP_403_FORBIDDEN)
-    
+
     user = request.user
     profile = user.profile
 
-    serializer = User_Profile_Update_Serializer(profile, data = request.data, partial = True)
-    
+    serializer = User_Profile_Update_Serializer(
+        profile, data=request.data, partial=True)
+
     if serializer.is_valid():
         serializer.save()
         return Response({'status': 'success'}, status=status.HTTP_200_OK)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
